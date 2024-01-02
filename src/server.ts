6f67fea7b6bd62142e7c7cfc3dad24c2fb6502c0
caio pereira <caiomp87@gmail.com>
2024-01-01 23:46:25 -0300

    develop a crud transaction api

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
+
+const app = fastify()
+
+app.register(cookie)
+app.register(transactionRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('server is running')
+  })
